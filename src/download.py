3ee50b757aa83845aed6c04a6b6c580687a0b1a7
Matias Gutierrez <magutierrez@fi.uba.ar>
2025-09-20 22:23:48 -0300

    Formateado con flake8

--- src/download.py
+++ src/download.py
@@ -1,19 +1,37 @@
 import argparse
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(prog = 'upload.py',description="File Transfer",epilog = 'TP N#1: File Transfer ')
+    parser = argparse.ArgumentParser(prog='upload.py',
+                                     description="File Transfer",
+                                     epilog='TP N#1: File Transfer ')
  
     group = parser.add_mutually_exclusive_group()
-    group.add_argument('-v', '--verbose', action='store_true', help="increase output verbosity")
-    group.add_argument('-q', '--quiet', action='store_true', help="decrease output verbosity")
-    parser.add_argument('-H', '--addr', type=str, default="127.0.0.1", help="server IP address")
-    parser.add_argument('-p', '--port', type=int, default=65432, help="server port")
-    parser.add_argument('-s', '--filepath', type=str, default="", help="dst destination file path")
-    parser.add_argument('-n', '--filename', type=str, default="", help="file name")
-    parser.add_argument('-r', '--protocol', type=str, default="", help="Stop & Wait or Selective Repeat[SW or SR]")
+    group.add_argument('-v', '--verbose',
+                       action='store_true',
+                       help="increase output verbosity")
+    group.add_argument('-q', '--quiet',
+                       action='store_true',
+                       help="decrease output verbosity")
+    parser.add_argument('-H', '--addr',
+                        type=str, default="127.0.0.1",
+                        help="server IP address")
+    parser.add_argument('-p', '--port',
+                        type=int,
+                        default=65432,
+                        help="server port")
+    parser.add_argument('-s', '--filepath',
+                        type=str, default="",
+                        help="dst destination file path")
+    parser.add_argument('-n', '--filename',
+                        type=str,
+                        default="",
+                        help="file name")
+    parser.add_argument('-r', '--protocol',
+                        type=str, default="",
+                        help="Stop & Wait or Selective Repeat[SW or SR]")
     args = parser.parse_args()
 
-    if args.verbose: 
+    if args.verbose:
         print(args.addr)
         print(args.port)
         print(args.filepath)
